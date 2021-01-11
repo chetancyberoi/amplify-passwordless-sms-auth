e16fcb186cbf992b5e8cef8ced05293aee1f42e6
chetancyberoi <76002216+chetancyberoi@users.noreply.github.com>
2021-01-11 13:54:27 +0530

    Update environment.ts

--- webClient/angular/src/environments/environment.ts
+++ webClient/angular/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  region: 'your region',
-  userPoolId: 'your user pool id',
-  userPoolWebClientId: 'your client id',
+  region: 'us-east-1',
+  userPoolId: 'us-east-1_0jZ87HVMA',
+  userPoolWebClientId: '7oqjlbecui0li1p1loopis8uo4',
 };
 
 /*
